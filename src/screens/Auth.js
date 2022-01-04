951fb8ad26da79fab9404082d27d9bc5a3379ff3
Juan Manuel Ramirez <juanmanuel.ramirez@tecso.coop>
2022-01-04 15:39:44 -0300

    Primer commit

---
+++ src/screens/Auth.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { StyleSheet ,View, Text, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import RegisterForm from '../components/Auth/RegisterForm';
+import logo from "../../assets/logo.png";
+import { layoutStyle } from "../styles";
+
+export default function Auth() {
+    const [showLogin, setShowLogin] = useState(false);
+
+    const changeForm = () => setShowLogin(!showLogin);
+
+    return (
+        <View style={layoutStyle.container}>
+            <Image style={styles.logo} source={logo} />
+            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "null"}>
+                {showLogin ? <Text>FormLogin</Text> : <RegisterForm changeForm={changeForm}/>}
+            </KeyboardAvoidingView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    logo: {
+        width: "100%",
+        height: 50,
+        resizeMode: "contain",
+        marginBottom:20
+    },
+});
