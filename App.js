913e6b4ca2f3facada5fa3a04066195e737a8358
Juan Manuel Ramirez <juanmanuel.ramirez@tecso.coop>
2022-01-05 09:05:42 -0300

    Guardado de token y seteado de auth.

--- App.js
+++ App.js
@@ -3,20 +3,29 @@ import { Text } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import AuthScreen from "./src/screens/Auth";
 import AuthContext from "./src/context/AuthContext";
+import { setTokenApi } from "./src/api/token";
 
 export default function App() {
   const [auth, setAuth] = useState(undefined);
 
   useEffect(() => {
     setAuth(null);
-  }, [])
+  }, []);
+
+  const login = (user) => {
+    setTokenApi(user.jwt);
+    setAuth({
+      token: user.jwt,
+      idUser: user.user._id
+    });
+  }
 
    // El useMemo lo que hace es escuchar en este caso a 'auth' 
    // y en caso que se modifique el valor, lo actualiza, de lo contrario no hace nada.
   const authData = useMemo(
     () => ({
       auth,
-      login: () => null,
+      login,
       logout: () => null,
     }),
     [auth]
@@ -26,7 +35,7 @@ export default function App() {
   return (
     <AuthContext.Provider value={authData}>
       <PaperProvider>
-        {auth ? <Text>Zona de Usuarios</Text> : <AuthScreen />}
+        {auth ? <Text style={{ flex: 1, justifyContent: "center", textAlign: "center" }}>Zona de Usuarios</Text> : <AuthScreen />}
       </PaperProvider>
     </AuthContext.Provider>
     
--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -4,12 +4,15 @@ import { TextInput, Button } from "react-native-paper";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import Toast from "react-native-root-toast";
+import useAuth from '../../hooks/useAuth';
 import { loginApi } from "../../api/user";
 import { formStyle } from "../../styles";
 
 export default function LoginForm(props) {
     const {changeForm} = props;
     const [loading, setLoading] = useState(false)
+    const { login } = useAuth();
+    
 
     const formik = useFormik({
         initialValues: initialValues(),
@@ -18,14 +21,15 @@ export default function LoginForm(props) {
             setLoading(true);
             try {
                 const response = await loginApi(formData);
-                console.log(response);
                 if(response.statusCode) throw "Error en el usuario o contraseña";
+                login(response);
             } catch (error) {
                 Toast.show(error, {
                     position: Toast.positions.CENTER,
                 });
                 setLoading(false);
             }
+            setLoading(false);
         }
     });
 
